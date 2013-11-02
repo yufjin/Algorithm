03cf9f7f82296dc58e86e9e342c616170a310526
yufjin <yufjin1015@gmail.com>
2013-11-03 01:07:14 +0800

    java gui review and learning

---
+++ GUILearing/src/yufjin/software/nju/edu/cn/SwingTesting.java
@@ -0,0 +1,14 @@
+package yufjin.software.nju.edu.cn;
+
+import javax.swing.JFrame;
+
+public class SwingTesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame f = new JFrame();
+        f.setSize( 300, 200 );
+        f.setVisible(true);
+	}
+
+}
